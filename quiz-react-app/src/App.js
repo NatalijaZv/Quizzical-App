71b34d7da067c1a491cc38dcd649fd5a822d0b28
NatalijaZv <natalijazver929@gmail.com>
2022-07-13 22:05:27 +0200

    Choose answer functionality
    Choose and change answer logic.

--- quiz-react-app/src/App.js
+++ quiz-react-app/src/App.js
@@ -1,35 +1,62 @@
-
 import React from "react";
-import Home from "./pages/Home"
-import Quiz from "./pages/Quiz"
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { nanoid } from "nanoid";
 
 export default function App() {
-  const [quizData, setQuizData] = React.useState("") 
-  console.log("test")
+  const [quizData, setQuizData] = React.useState("");
 
-  React.useEffect(()=>{
-    console.log("effect run")
+  console.log("quizData");
+  function shuffle(array){
+    let currentIndex = array.length
+    while(currentIndex !== 0){
+       let randomIndex = Math.floor(Math.random()*currentIndex)
+       currentIndex--
+       [array[currentIndex],array[randomIndex]]=[array[randomIndex],array[currentIndex]]
+    }
+    return array
+}
+
+  React.useEffect(() => {
+    console.log("effect run");
     fetch("https://opentdb.com/api.php?amount=5")
-    .then(res => res.json())
-    .then(data=>(
-        setQuizData(data.results)
-            ))
-      console.log(quizData)
-  },[])
-  
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setQuizData(
+          data.results.map((elem) => ({
+            answers: shuffle([
+              {
+                answer: elem.correct_answer,
+                isSelected: false,
+                isCorrect: true,
+                id:nanoid()
+              },
+              ...elem.incorrect_answers.map((answer) => ({
+                answer: answer,
+                isSelected: false,
+                isCorrect: false,
+                id: nanoid()
+              })),
+            ]),
+            question: elem.question,
+          }))
+        );
+      });
+
+    console.log(quizData);
+  }, []);
 
   // console.log(quizData[1].category)
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-        <Route index path = "home" element={<Home />} />
-          <Route path="quiz" element={<Quiz quizData = {quizData}/>} />
+          <Route index path="home" element={<Home />} />
+          <Route path="quiz" element={<Quiz quizData={quizData} setQuizData={setQuizData} />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-
--- quiz-react-app/src/pages/Quiz.js
+++ quiz-react-app/src/pages/Quiz.js
@@ -1,59 +1,58 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom"
-import { nanoid } from 'nanoid'
+import { Outlet, Link } from "react-router-dom";
+import { nanoid } from "nanoid";
 
+export default function Quiz(props) {
 
-export default function Quiz (props){
-    console.log(props)
-    //SHUFFLE FUNCTION
-    function shuffle(array){
-        let currentIndex = array.length
-        while(currentIndex !== 0){
-           let randomIndex = Math.floor(Math.random()*currentIndex)
-           currentIndex--
-           [array[currentIndex],array[randomIndex]]=[array[randomIndex],array[currentIndex]]
-        }
-        return array
-    }
+  function chooseAnswer(id, elem) {
+    //SET NEW QUIZDATA
+    props.setQuizData((prev) => {
       
-
-    const quizElement = props.quizData.map(elem => {
-        // console.log(elem.question)
-        function chooseAnswer(index,e){
-            e.preventDefault()
-            console.log(index)
-            e.target.classList.add("choosenAnswer")
-            for (let i = 0; i<answers.length;i++){
-                if(index === answers.indexOf()){
-                    
-                }
+      const prevArr = prev.map((data) => {
+        const question = data.question;
+        const currentIdx = prev.indexOf(elem);
+    
+        if (currentIdx === prev.indexOf(data)) {
+          let answers = data.answers.map((answer) => {
+            if (answer.id === id) {
+              return { ...answer, isSelected: !answer.isSelected };
+            } else {
+              return { ...answer, isSelected: false };
             }
-            
+          });
+          return { answers, question };
         }
-        const answers = []
-        answers.push(...elem.incorrect_answers)
-        answers.push(elem.correct_answer)
-        shuffle(answers)
-        let answersElem = answers.map(answer=> {
-            return (<span key={nanoid()}>
-                <span onClick={(e)=>chooseAnswer(answers.indexOf(answer),e)} className="answer">{answer}</span>
-            </span>
-                    
-                )
-        })
-        return <div key={nanoid()}>
-            <h3>{elem.question}</h3>
-            <div>
-                {answersElem}
-            </div>
-        </div>
-    })  
-    return(
-        <div>
-            {quizElement}
-            <Link to="/home">Home</Link>
-        </div>
-        
+        return data;
+      });
+      return prevArr;
+    });
+  }
+  const quizElement = props.quizData.map((elem) => {
+    
+    let answersElem = elem.answers.map((ans) => {
+      return (
+        <span key={ans.id}>
+          <span
+            onClick={() => chooseAnswer(ans.id, elem)}
+            className={`answer ${ans.isSelected ? "isSelected" : ""}`}
+          >
+            {ans.answer}
+          </span>
+        </span>
+      );
+    });
+    return (
+      <div key={nanoid()}>
+        <h3>{elem.question}</h3>
+        <div>{answersElem}</div>
+      </div>
+    );
+  });
 
-    )
+  return (
+    <div>
+      {quizElement}
+      <Link to="/home">Home</Link>
+    </div>
+  );
 }
